071bb96631206ff5df15a997448fa4e06a3b8217
LeonardSuli <leonardsuli1991@gmail.com>
2024-06-09 18:13:16 +0200

    add faker in seeder

--- database/seeders/PhotoSeeder.php
+++ database/seeders/PhotoSeeder.php
@@ -4,14 +4,24 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Photo;
+use Illuminate\Support\Str;
 
 class PhotoSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $photo = new Photo();
+            $photo->title = $faker->words(4, true);
+            $photo->description = $faker->text();
+            $photo->upload_image = $faker->imageUrl(600, 400, 'Photos', true, $photo->title, true, 'jpg');
+            $photo->slug = Str::of($photo->title)->slug('-');
+            $photo->save();
+        }
     }
 }
